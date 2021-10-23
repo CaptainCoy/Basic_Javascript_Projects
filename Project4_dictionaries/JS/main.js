4aaa5103af5c065c90fd636c2e190284673345e7
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2021-10-23 14:40:10 -0600

    commit

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -1,11 +1,11 @@
 function my_Dictionary() {
-    var Animal={
+    var Animal={ //here is my dictionary, about the same as the course had only modified to match one of my dogs
         species:"Dog",
         Color:"Black/Tan",
         Breed:"Poodle",
         Age:1,
         Sound:"Woof!"        
     };
-    delete Animal.Sound;
+    delete Animal.Sound; //here is me using the delete function
     document.getElementById("Dictionary").innerHTML=Animal.Sound;
 }
