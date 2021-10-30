4b06e7b711af778906ed7e0beefe2ac74310c608
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2021-10-30 17:12:38 -0600

    project7

---
+++ Project7_scope_time_function/JS/Main.js
@@ -0,0 +1,10 @@
+var A=12
+function Global_var_Example() {
+    console.log(30+A);
+}
+function Local_var_Example() {
+    var B=30
+    console.log(A+B);
+}
+Global_var_Example();
+Local_var_Example();
